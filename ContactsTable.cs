9c2edac9e8dfea2807046b477700270f4660a661
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-27 11:18:46 -0300

    Abstract class created for Contacts table.

---
+++ ContactsTable.cs
@@ -0,0 +1,20 @@
+public abstract class ContactsTable
+{
+    private int contactId;
+    private string name;
+    private string city;
+    private string state;
+    private string phone_number;
+
+    public int ContactId { get => contactId; set => contactId = value; }
+    public string Name { get => name; set => name = value; }
+    public string City { get => city; set => city = value; }
+    public string State { get => state; set => state = value; }
+    public string Phone_number { get => phone_number; set => phone_number = value; }
+
+    public abstract void Insert();
+    public abstract void Delete();
+    public abstract void Update();
+    public abstract void Select();
+
+}
