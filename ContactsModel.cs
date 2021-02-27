7a301e9f44fa5ccf472ab95f35618b9c3bed7f69
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-27 10:55:15 -0300

    The model for Contacts table was created.

---
+++ ContactsModel.cs
@@ -0,0 +1,11 @@
+namespace MyContacts
+{
+    public class ContactsModel
+    {
+        public int ContactId{get; set;}
+        public string Name{get; set;}
+        public string City{get; set;}
+        public string State{get; set;}
+        public string Phone_number{get; set;}
+    }
+}
