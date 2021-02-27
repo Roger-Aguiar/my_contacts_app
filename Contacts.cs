7be5de54ebf18f3ae27c586c149bf81858923e30
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-27 14:13:48 -0300

    The Select method was implemented.

--- Contacts.cs
+++ Contacts.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Linq;
 
 namespace MyContacts
 {
     class Contacts : ContactsOperations
     {
+        DatabaseContacts contact = new DatabaseContacts();
+
+        public Contacts()
+        {
+
+        }
+        
         public Contacts(string name, string city, string state, string phone_number)
         {
             Name = name;
             City = city;
             State = state;
-            Phone_number = $"{long.Parse(phone_number):(00)00000-0000}";
+            Phone_number = $"{long.Parse(phone_number):(00)0-0000-0000}";
         }
 
         public override void Delete()
@@ -19,16 +27,30 @@ namespace MyContacts
         
         public override void Insert()
         {            
-            using(DatabaseContacts contact = new DatabaseContacts())
+            using(contact)
             {
                 contact.Add(new Contact{Name = Name, City = City, State = State, Phone_number = Phone_number});
                 contact.SaveChanges();
             }
         }
 
-        public override void Select()
+        public override string Select()
         {
-            throw new NotImplementedException();
+            string output_contacts;
+
+            output_contacts = "My contacts\n";
+            
+            using(contact)
+            {
+                var contactList = contact.Contacts.OrderBy(contacts => contacts.Name);
+                foreach (var currentContact in contactList)
+                {
+                    output_contacts += "Name: " + currentContact.Name + "\nCity: " + currentContact.City + 
+                                       "\nState: " + currentContact.State + "\nPhone number: " + currentContact.Phone_number +
+                                       "\n=============================================\n";                
+                }
+            }
+            return output_contacts;
         }
 
         public override string ToString()
