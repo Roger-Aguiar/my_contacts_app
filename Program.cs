94e9cfe3598e38ed1e288184e6a18b3971632217
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-27 14:14:04 -0300

    The menu was created.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,55 @@ namespace MyContacts
     class Program
     {
         static void Main(string[] args)
+        {              
+            int option = Menu();
+
+            while(option != 5)
+            {
+                Console.Clear();
+
+                switch(option)
+                {
+                    case 1:
+                        Insert(); 
+                        Console.Clear();                       
+                        Select();                        
+                        break;
+                    case 2:
+                        Delete();                    
+                        Select();                        
+                        break;
+                    case 3:
+                        Update();                        
+                        Select();                        
+                        break;
+                    case 4:
+                        Select();                        
+                        break;                    
+                }
+                option = Menu();
+            };
+        }       
+
+        static int Menu()
         {
+            Console.WriteLine("1 - Insert new contact");
+            Console.WriteLine("2 - Delete contact");
+            Console.WriteLine("3 - Update contact");
+            Console.WriteLine("4 - Display contacts");
+            Console.WriteLine("5 - Exit");
+            Console.Write("Choose one option: ");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        static void Delete()
+        {
+
+        }
+
+        static void Insert()
+        {
+            Console.Clear();
             Console.WriteLine("CONTACTS DATABASE\n");
             Console.WriteLine("Enter the following data:\n");
 
@@ -20,10 +68,22 @@ namespace MyContacts
 
             Console.Write("Enter the phone number (Just number):");
             string phone_number = Console.ReadLine();
-
+            
             Contacts contact = new Contacts(name, city, state, phone_number);
             contact.Insert();
-            Console.WriteLine("\nOperation has been completed!");
+
+            Console.WriteLine("Operation has been completed!");
+        }
+
+        static void Select()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine(contacts.Select());
+        }
+
+        static void Update()
+        {
+
         }
     }
 }
