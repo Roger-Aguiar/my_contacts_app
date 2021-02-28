1859f69f7a36fe785a94c3e1082349a145e8a124
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-28 20:34:44 -0300

    The constructors were removed and the propeties had its modifiers changed from protected to public.

--- ContactsOperations.cs
+++ ContactsOperations.cs
@@ -1,35 +1,11 @@
 public abstract class ContactsOperations
 {
-    protected int ContactId { get; set; }
-    protected string Name { get; set; }
-    protected string City { get; set; }
-    protected string State { get; set; }
-    protected string Phone_number { get; set; }
-
-    protected ContactsOperations(){}
-
-    protected ContactsOperations(int contactId)
-    {
-        ContactId = contactId;
-    }
-
-    protected ContactsOperations(int contactId, string name, string city, string state, string phone_number)
-    {
-        ContactId = contactId;
-        Name = name;
-        City = city;
-        State = state;
-        Phone_number = phone_number;
-    }
-
-    protected ContactsOperations(string name, string city, string state, string phone_number)
-    {        
-        Name = name;
-        City = city;
-        State = state;
-        Phone_number = phone_number;
-    }
-
+    public int ContactId { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Phone_number { get; set; }
+    
     public abstract void Insert();
     public abstract void Delete();
     public abstract void Update();
