40b0e3e82e3a3854136f565c6b03513b629509bf
Roger <roger.silvaaguiar@yahoo.com.br>
2021-02-28 15:50:28 -0300

    The instance variables were removed from the class ContactsOperations.cs and the propeties were modified.

--- ContactsOperations.cs
+++ ContactsOperations.cs
@@ -1,16 +1,10 @@
 public abstract class ContactsOperations
 {
-    private int contactId;
-    private string name;
-    private string city;
-    private string state;
-    private string phone_number;
-
-    public int ContactId { get => contactId; set => contactId = value; }
-    public string Name { get => name; set => name = value; }
-    public string City { get => city; set => city = value; }
-    public string State { get => state; set => state = value; }
-    public string Phone_number { get => phone_number; set => phone_number = value; }
+    public int ContactId { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Phone_number { get; set; }
 
     public abstract void Insert();
     public abstract void Delete();
